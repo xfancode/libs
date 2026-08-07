40a7838cd8632a70cd06eb64f4843951291248b0
xfancode <userappcontentmobitsa@gmail.com>
2026-08-07 03:07:16 +0300

    Add files via upload

---
+++ ls.js
@@ -0,0 +1,4 @@
+lsLoad=a=>{try{return JSON.parse(localStorage.getItem(a))}catch(b){return localStorage.getItem(a)}};
+lsSave=(a,b)=>localStorage.setItem(a,b&&typeof b=='object'?JSON.stringify(b):b);
+lsRemove=a=>localStorage.removeItem(a);
+lsClear=_=>localStorage.clear();
