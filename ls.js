1f8fb95525b24f87bb315ac5ea766bc53542201d
xfancode <userappcontentmobitsa@gmail.com>
2026-08-07 03:07:50 +0300

    ls.js [JavaScript]

--- ls.js
+++ ls.js
@@ -1,3 +1,4 @@
+// ls.js by xfan
 lsLoad=a=>{try{return JSON.parse(localStorage.getItem(a))}catch(b){return localStorage.getItem(a)}};
 lsSave=(a,b)=>localStorage.setItem(a,b&&typeof b=='object'?JSON.stringify(b):b);
 lsRemove=a=>localStorage.removeItem(a);
